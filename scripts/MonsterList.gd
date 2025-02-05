3b9cbafa3934d419a952f4c7ae5eeae5229ecb78
Nilava <nilavasaha10@gmail.com>
2025-02-04 22:45:38 -0600

    some changes

--- scripts/MonsterList.gd
+++ scripts/MonsterList.gd
@@ -1,9 +1,12 @@
 extends Resource
 class_name MonsterList
 
+@export var background: Texture2D
 @export var monsters: Array[MonsterData] = []
 
 func get_size() -> int:
 	return monsters.size()
 func get_monster(index: int) -> MonsterData:
 	return monsters[index]
+func get_background() -> Texture2D:
+	return background
--- scripts/action_scene_scripts/action_scene.gd
+++ scripts/action_scene_scripts/action_scene.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var player_health_bar: ProgressBar
+@export var enemy_dictionary: MonsterList
+@export var background:Sprite2D
 
 var player_health = Global.get_player_health()
 var player_damage = Global.get_player_damage()
@@ -9,6 +11,7 @@ var player_tenacity = Global.get_player_tenacity()
 func _on_ready() -> void:
 	player_health_bar.max_value = player_health
 	player_health_bar.value = player_health
+	background.texture = enemy_dictionary.get_background()
 	
 func _process(delta:float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
